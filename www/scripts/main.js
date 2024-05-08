5432c142af34fa7c1fa529d79018bd87b7f4321d
weastler <32074613+weastler@users.noreply.github.com>
2024-05-08 12:18:13 -0700

    restructure the content blocks a bit and add some basic reflow using CSS grid and media breakpoints.
    restructure the content blocks a bit and add some basic reflow using CSS grid and media breakpoints.
    
    Added a basic code copy button for the code blocks.

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,3 +1,28 @@
 (function () {
+    const codeBlocks = document.querySelectorAll('.code-box');
+
+    codeBlocks.forEach((codeBlock) => {
+        const codeBlockCopyButton = codeBlock.querySelector('.copy-button');
+
+        if(codeBlockCopyButton) {
+            codeBlockCopyButton.addEventListener('click', (e) => {
+                const parent = e.target.parentElement;
+                codeBlock.dispatchEvent(new CustomEvent('copyCode', { bubbles: true }));
+            });
+            
+            codeBlock.addEventListener('copyCode', async (e) => {
+                const codeContent = codeBlock.querySelector('.code-content');
+
+                try {
+                    await navigator.clipboard.writeText(codeContent.innerText);
+                    console.log('Code copied to clipboard');
+                    console.log(codeContent.innerText);
+                }
+                catch (err) {
+                    console.error('Failed to copy text to clipboard: ', err);
+                }
+            });
+        }
+    });
 
 }());
